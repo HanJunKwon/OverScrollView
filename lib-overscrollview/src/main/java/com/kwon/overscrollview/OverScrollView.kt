938d7767ffb881c20ae78428fe62a91c8905b790
Han jun Kwon <hanjun.kwon.hs@gmail.com>
2025-05-29 10:22:34 +0900

    OverScrollView  ACTION_POINTER_UP 이벤트 추가

--- lib-overscrollview/src/main/java/com/kwon/overscrollview/OverScrollView.kt
+++ lib-overscrollview/src/main/java/com/kwon/overscrollview/OverScrollView.kt
@@ -1,5 +1,6 @@
 package com.kwon.overscrollview
 
+import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
@@ -8,21 +9,36 @@ import android.view.MotionEvent
 import android.view.MotionEvent.ACTION_CANCEL
 import android.view.MotionEvent.ACTION_DOWN
 import android.view.MotionEvent.ACTION_MOVE
+import android.view.MotionEvent.ACTION_POINTER_UP
 import android.view.MotionEvent.ACTION_UP
-import android.view.View
 import android.view.animation.LinearInterpolator
 import android.widget.ScrollView
 import com.kwon.overscrollview.OverScrollView.SCROLL_DIRECTION.Companion.SCROLL_DIRECTION_BOTTOM
 import com.kwon.overscrollview.OverScrollView.SCROLL_DIRECTION.Companion.SCROLL_DIRECTION_TOP
 import kotlin.math.abs
 
+
 class OverScrollView(context: Context, attrs: AttributeSet): ScrollView(context, attrs) {
+
+    /**
+     * 사용자가 스크롤을 할 때, 스크롤뷰의 상단 또는 하단에서 오버스크롤이 발생했는지 여부
+     */
     private var isOverScroll = false
 
+    /**
+     * 오버스크롤 이벤트가 발생할 때 스크롤 중인 방향
+     * - [SCROLL_DIRECTION_TOP] : 스크롤뷰의 상단에서 오버스크롤이 발생
+     * - [SCROLL_DIRECTION_BOTTOM] : 스크롤뷰의 하단에서 오버스크롤이 발생
+     */
     @SCROLL_DIRECTION
     private var overScrollDirection = SCROLL_DIRECTION_TOP
 
-    private var scrollHeight: Int = -1
+    /**
+     * 스크롤뷰의 높이.
+     *
+     * [onMeasure] 메소드에서 정확한 높이가 계산 됐을 때 설정됨.
+     */
+    private var scrollViewHeight: Int = -1
 
     private var overScrollStartY = -1f
 
@@ -31,86 +47,84 @@ class OverScrollView(context: Context, attrs: AttributeSet): ScrollView(context,
     private var overScrollRecoveredValueAnimator = ValueAnimator.ofInt().apply {
         duration = 250L
         interpolator = LinearInterpolator()
+        addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: android.animation.Animator) {
+                super.onAnimationEnd(animation)
+                isOverScroll = false
+                overScrollStartY = -1f
+                setOverScrollPaddingInternal(0)
+            }
+        })
+        addUpdateListener { animation ->
+            val value = animation.animatedValue as Int
+            if (overScrollDirection == SCROLL_DIRECTION_TOP) {
+                setPadding(0, value, 0, 0)
+            } else {
+                setPadding(0, 0, 0, value)
+            }
+        }
     }
 
     init {
         overScrollMode = OVER_SCROLL_NEVER
     }
 
-    override fun onGenericMotionEvent(event: MotionEvent?): Boolean {
-        Log.d(">>>", "onGenericMotionEvent() :: ${event?.action}")
-        return super.onGenericMotionEvent(event)
-    }
-
-    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
-        Log.d(">>>", "onInterceptTouchEvent() :: ${ev?.action}")
-        return super.onInterceptTouchEvent(ev)
-    }
-
     override fun onTouchEvent(ev: MotionEvent?): Boolean {
-//        Log.d(">>>", "onTouchEvent() :: action: ${ev?.action}, rawY: ${ev?.rawY}, y: ${ev?.y}")
-
-        if (isOverScroll && ev?.action == ACTION_MOVE) {
-            if (overScrollStartY == -1f) {
-                overScrollStartY = ev.y
+        when (ev?.action) {
+            ACTION_DOWN -> {
+                if (overScrollRecoveredValueAnimator.isRunning) {
+                    setOverScrollPaddingInternal(overScrollRecoveredValueAnimator.animatedValue as Int)
+                    overScrollRecoveredValueAnimator.pause()
+                }
             }
 
-            when (overScrollDirection) {
-                SCROLL_DIRECTION_TOP -> {
-                    overScrollPadding = (abs(overScrollStartY - ev.y).toInt())/2
-                    Log.d(">>>", "overScrollPadding: ${overScrollPadding}")
+            ACTION_UP, ACTION_CANCEL -> {
+                // 오버 스크롤을 원래 상태로 되돌리는 애니메이션 시작
+                setScrollRecoverValueAnimation(overScrollPadding, 0)
+                startOverScrollRecoverAnimation()
+            }
 
-                    setPadding(0, overScrollPadding, 0, 0)
+            ACTION_MOVE -> {
+                if (isOverScroll) {
+                    if (overScrollStartY == -1f) {
+                        overScrollStartY = ev.y
+                    }
+
+                    when (overScrollDirection) {
+                        SCROLL_DIRECTION_TOP -> {
+                            val distance = abs(overScrollStartY - ev.y).toInt()
+                            overScrollPadding = distance / 2
+                            setPadding(0, overScrollPadding, 0, 0)
 //                    Log.d(">>>", "onTouchEvent() :: SCROLL DIRECTION TOP")
-                }
-                SCROLL_DIRECTION_BOTTOM -> {
-                    overScrollPadding = (abs(overScrollStartY - ev.y).toInt())/2
-                    setPadding(0, 0, 0, overScrollPadding)
+                        }
+
+                        SCROLL_DIRECTION_BOTTOM -> {
+                            overScrollPadding = (abs(overScrollStartY - ev.y).toInt()) / 2
+                            setPadding(0, 0, 0, overScrollPadding)
 //                    Log.d(">>>", "onTouchEvent() :: SCROLL DIRECTION BOTTOM")
+                        }
+                    }
+
+                    return false
                 }
             }
 
-            return false
-        } else if (isOverScroll && (ev?.action == ACTION_CANCEL || ev?.action == ACTION_UP)) {
-            overScrollRecoveredValueAnimator.apply {
-                setIntValues(overScrollPadding, 0)
-                addUpdateListener { animation ->
-                    if (overScrollDirection == SCROLL_DIRECTION_TOP) {
-                        setPadding(0, animation.animatedValue as Int, 0, 0)
-                    } else {
-                        setPadding(0, 0, 0, animation.animatedValue as Int)
-                    }
-
-                    if (animation.animatedValue as Int == 0) {
-                        isOverScroll = false
-                        overScrollStartY = -1f
-                        overScrollPadding = 0
-                    }
-                }
-                start()
-            }
-        } else if (ev?.action == ACTION_DOWN) {
-            if (overScrollRecoveredValueAnimator.isRunning) {
-                overScrollPadding = overScrollRecoveredValueAnimator.animatedValue as Int
-                overScrollRecoveredValueAnimator.pause()
+            ACTION_POINTER_UP -> {
+                setScrollRecoverValueAnimation(overScrollPadding, 0)
+                startOverScrollRecoverAnimation()
             }
         }
 
         return super.onTouchEvent(ev)
     }
 
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        return super.dispatchTouchEvent(ev)
-    }
-
     override fun onOverScrolled(scrollX: Int, scrollY: Int, clampedX: Boolean, clampedY: Boolean) {
         super.onOverScrolled(scrollX, scrollY, clampedX, clampedY)
-//        Log.d(">>>", "onOverScrolled() :: scrollX: $scrollX, scrollY: $scrollY, clampedX: ${clampedX}, clampedY: ${clampedY}")
 
-        if (scrollHeight == -1) return
+        if (scrollViewHeight == -1) return
 
         if (clampedY) {
-            isOverScroll = (scrollY == 0 || scrollY == scrollHeight)
+            isOverScroll = (scrollY == 0 || scrollY == scrollViewHeight)
             overScrollDirection = if (scrollY == 0) SCROLL_DIRECTION_TOP else SCROLL_DIRECTION_BOTTOM
         }
     }
@@ -120,18 +134,20 @@ class OverScrollView(context: Context, attrs: AttributeSet): ScrollView(context,
 
         val heightMode = MeasureSpec.getMode(heightMeasureSpec)
         if (heightMode == MeasureSpec.EXACTLY) {
-            scrollHeight = MeasureSpec.getSize(heightMeasureSpec)
+            scrollViewHeight = MeasureSpec.getSize(heightMeasureSpec)
         }
     }
 
-    override fun onNestedFling(
-        target: View,
-        velocityX: Float,
-        velocityY: Float,
-        consumed: Boolean
-    ): Boolean {
-        Log.d(">>>", "onNestedFling() :: velocityX: $velocityX, velocityY: $velocityY")
-        return super.onNestedFling(target, velocityX, velocityY, consumed)
+    private fun setOverScrollPaddingInternal(padding: Int) {
+        this.overScrollPadding = padding
+    }
+
+    private fun setScrollRecoverValueAnimation(vararg values: Int) {
+        overScrollRecoveredValueAnimator.setIntValues(*values)
+    }
+
+    private fun startOverScrollRecoverAnimation() {
+        overScrollRecoveredValueAnimator.start()
     }
 
     annotation class SCROLL_DIRECTION {
